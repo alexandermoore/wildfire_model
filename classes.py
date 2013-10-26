473e682014d2eaea8fba5f3f34d261346fcf93ca
alexandermoore <alexmoore700@gmail.com>
2013-10-26 03:18:47 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -8,7 +8,7 @@ Email: alexandermoore at college.harvard.edu
 
 This file handles the simulation's inner workings.
 The simulation is based on the model described by Almeida and Macau here: 
-http://iopscience.iop.org/1742-6596/285/1/012038/pdf/1742-6596_285_1_012038.pdf
+http://iopscience.iop.org/1742-6596/285/1/012038
 Additional complicatons such as wind and probabilities that vary per tree were added by me.
 """
 
